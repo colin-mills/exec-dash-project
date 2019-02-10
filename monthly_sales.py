d671ab7f819c50c48fff4d122eae9084c2a53e17
Colin Mills <cgm71@georgetown.edu>
2019-02-10 15:18:20 -0500

    reads a csv file with given filepath to dirrectory

--- monthly_sales.py
+++ monthly_sales.py
@@ -1,11 +1,25 @@
 # monthly_sales.py
 
+import datetime
+import csv
+
+
 # TODO: import some modules and/or packages here
 
 # TODO: write some Python code here to produce the desired functionality...
 
 print("-----------------------")
 print("MONTH: March 2018")
+
+csv_file_path = "data/sales-201710.csv" # a relative filepath
+files = []
+
+with open(csv_file_path, "r") as csv_file: # "r" means "open the file for reading"
+    reader = csv.DictReader(csv_file) # assuming your CSV has headers
+    for row in reader:
+        d = {"date": row["date"], "product": row["product"], "unit_price": float(row["unit price"]), "units_sold" : int(row["units sold"]), "sales_price" : float(row["sales price"])}
+        #print(type(d), d["name"], d["price"])
+        files.append(d)
 
 print("-----------------------")
 print("CRUNCHING THE DATA...")
